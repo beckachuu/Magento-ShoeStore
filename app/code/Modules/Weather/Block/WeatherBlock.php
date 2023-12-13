4e5dcf79b46dcd94d690de52b7d3b102b87f7004
beckachuu <minhtrang072121@gmail.com>
2023-12-13 13:50:41 +0000

    feat: add weather block

---
+++ app/code/Modules/Weather/Block/WeatherBlock.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Modules\Weather\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class WeatherBlock extends Template
+{
+  public function __construct(\Magento\Framework\View\Element\Template\Context $context) {
+  		parent::__construct($context);
+  }
+}
