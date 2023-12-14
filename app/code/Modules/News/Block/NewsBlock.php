c26ea94d7dea8f215c58921baecdb3c3c4b67abe
beckachuu <minhtrang072121@gmail.com>
2023-12-15 04:25:16 +0700

    feat: add module for business news

---
+++ app/code/Modules/News/Block/NewsBlock.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\News\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class NewsBlock extends Template
+{
+	public function __construct(\Magento\Framework\View\Element\Template\Context $context)
+	{
+		parent::__construct($context);
+	}
+}
